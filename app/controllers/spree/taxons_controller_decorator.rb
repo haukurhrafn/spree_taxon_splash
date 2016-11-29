8fbc07337d0eda2b6a388be9d37683a5d07621d9
Haukur Thorsteinsson <haukur@haukur.net>
2016-11-29 22:02:36 +0000

    Spree 3.0 support

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -4,12 +4,17 @@ Spree::TaxonsController.class_eval do
   old_show = instance_method(:show)
 
   define_method(:show) do
-    @taxon = Spree::Taxon.find_by_permalink!(params[:id])
-
+    @taxon = Spree::Taxon.friendly.find(params[:id])
+    
     if @taxon.taxon_splash.nil?
       old_show.bind(self).()
     else
+      old_show.bind(self).()
       render :template => 'spree/taxons/splash'
     end
   end
+  
+  #def taxon_params
+  #  params.require(:user).permit(:taxon_id, :content)
+  #end
 end
--- app/overrides/add_slides_to_admin_configurations_sidebar_menu.rb
+++ app/overrides/add_slides_to_admin_configurations_sidebar_menu.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => 'spree/admin/shared/_configuration_menu',
+Deface::Override.new(:virtual_path => 'spree/admin/shared/sub_menu/_configuration',
                      :name => 'add_taxon_splashes_to_admin_configurations_sidebar_menu',
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
                      :text => %q{
